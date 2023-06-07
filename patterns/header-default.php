9102680acfd619a6fc9d9cf2ab0a18213e0bdd82
Erik Joling <info@ejoweb.nl>
2023-06-07 15:40:49 +0200

    Remove borders and finetune layout

--- patterns/header-default.php
+++ patterns/header-default.php
@@ -6,8 +6,8 @@
  * Inserter: no
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|40"}}},"backgroundColor":"base","textColor":"primary","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"left"}} -->
-<div class="wp-block-group has-primary-color has-base-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:site-logo {"width":147,"shouldSyncIcon":true} /-->
+<!-- wp:group {"textColor":"primary","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group has-primary-color has-text-color"><!-- wp:site-title {"fontSize":"x-large"} /-->
 
-<!-- wp:navigation {"ref":38,"textColor":"contrast","icon":"menu","overlayBackgroundColor":"base","overlayTextColor":"contrast","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"},"style":{"spacing":{"blockGap":"0"}}} /--></div>
+<!-- wp:navigation {"ref":63,"overlayMenu":"always","icon":"menu","overlayBackgroundColor":"base","overlayTextColor":"contrast","layout":{"type":"flex","justifyContent":"center","flexWrap":"wrap"},"style":{"spacing":{"blockGap":"0"}}} /--></div>
 <!-- /wp:group -->
